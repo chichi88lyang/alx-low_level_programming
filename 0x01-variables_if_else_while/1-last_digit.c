347ce575c26c0bc1c4a96075ae4044740818530c
chichi88lyang <cash88lyange@gmail.com>
2022-04-09 05:43:47 -0700

    harder

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main
- *
- * Return: Always (Success)
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
