a29609568017750d02fb241a645b2d49ce78edbb
chichi88lyang <cash88lyange@gmail.com>
2022-04-26 18:20:22 -0700

    My first commit

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,45 +1,21 @@
 #include "main.h"
-#include <stdio.h>
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ *_memset - fill a block of memory with a specific value
+ *@s: starting address of memory to be filled
+ *@b: the desired value
+ *@n: number of bytes to be changed
  *
- * Return: Nothing.
+ *Return: changed array with new value for n bytes
  */
-void simple_print_buffer(char *buffer, unsigned int size)
+char *_memset(char *s, char b, unsigned int n)
 {
-  unsigned int i;
+	int i = 0;
 
-  i = 0;
-  while (i < size)
-    {
-      if (i % 10)
+	while (n > 0)
 	{
-	  printf(" ");
+		s[i] = b;
+		i++;
+		n--;
 	}
-      if (!(i % 10) && i)
-	{
-	  printf("\n");
-	}
-      printf("0x%02x", buffer[i]);
-      i++;
-    }
-  printf("\n");
-}
-
-/**
- * main - check the code 
- *
- * Return: Always 0.
- */
-int main(void)
-{
-  char buffer[98] = {0x00};
-
-  simple_print_buffer(buffer, 98);
-  _memset(buffer, 0x01, 95);
-  printf("-------------------------------------------------\n");
-  simple_print_buffer(buffer, 98);    
-  return (0);
+	return (s);
 }
