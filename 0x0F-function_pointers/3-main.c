3f05aea6a01fba970a75fc2e4c4625b8becff127
focusTech1 <focusitstudio@gmail.com>
2022-05-11 17:21:19 -0700

    My commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,27 +1,36 @@
-#ifndef CALC_H
-#define CALC_H
-
-/*
- * Desc: Header file containing all structures and
- *       prototypes used by the 3-main.c program.
- */
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * struct op - A struct op.
- * @op: The operator.
- * @f: The associated function.
+ * main - program to perform operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0
  */
-typedef struct op
+
+int main(int argc, char *argv[])
 {
+	int num1, num2;
 	char *op;
-	int (*f)(int a, int b);
-} op_t;
 
-int op_add(int a, int b);
-int op_sub(int a, int b);
-int op_mul(int a, int b);
-int op_div(int a, int b);
-int op_mod(int a, int b);
-int (*get_op_func(char *s))(int, int);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-#endif
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
+}
