d3efd744bed9c930daa0bdf5a699fdbdde9bc518
chichi88lyang <cash88lyangenda@gmail.com>
2022-05-23 13:11:06 -0700

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,42 +1,45 @@
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a  new node at a given position.
- * @head: The first element of the list.
- * @idx: The position of insertion.
- * @n: The data of the new node.
- * Return: The address of the new node if success.
- *	   NULL otherwise.
+ * insert_nodeint_at_index - Inserts a new node to a listint_t
+ *                           list at a given position.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ * @idx: The index of the listint_t list where the new
+ *       node should be added - indices start at 0.
+ * @n: The integer for the new node to contain.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *tmp, *node;
-	unsigned int i = 0;
+	listint_t *new, *copy = *head;
+	unsigned int node;
 
-	node = malloc(sizeof(listint_t));
-	if (node)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (idx == 0)
 	{
-		node->n = n;
-		if (idx == 0)
-		{
-			node->next = *head;
-			head = &node;
-		}
-		else
-		{
-			tmp = *head;
-			while ((tmp != NULL) && (i  < idx))
-			{
-				tmp = tmp->next;
-				i++;
-			}
-			if (i  < idx)
-			{
-				node->next = tmp->next;
-				tmp->next = node;
-			}
-		}
+		new->next = copy;
+		*head = new;
+		return (new);
 	}
-	return (node);
+
+	for (node = 0; node < (idx - 1); node++)
+	{
+		if (copy == NULL || copy->next == NULL)
+			return (NULL);
+
+		copy = copy->next;
+	}
+
+	new->next = copy->next;
+	copy->next = new;
+
+	return (new);
 }
