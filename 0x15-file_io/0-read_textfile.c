ee0d303aa294ddde5b374a13c1f3fd20e9ce9e7a
chichi88lyang <cash88lyangenda@gmail.com>
2022-06-15 13:03:03 +0000

    commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,6 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
  * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
